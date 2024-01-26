cffe168c58fcd27eec67474e5d4277ed389b8b38
Your Name <you@example.com>
2024-01-26 14:59:26 +0700

    completed

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -17,3 +17,19 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &a,int &b,int &c,int &d){
+	int A[] = {a, b, c, d};
+    int n = sizeof(A) / sizeof(A[0]);
+
+    for (int i = n - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        swap(A[i], A[j]);
+    }
+	
+    a = A[0];
+    b = A[1];
+    c = A[2];
+    d = A[3];
+
+}
